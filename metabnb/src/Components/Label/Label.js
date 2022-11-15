bf9957dd59933ebb35fff70b87671754a0dd607d
kevinmbuicode <kevinmushan@gmail.com>
2022-11-15 12:50:53 +0300

    Chore: cleaned label

--- metabnb/src/Components/Label/Label.js
+++ metabnb/src/Components/Label/Label.js
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import React from "react";
 import OpenSea from "../../Images/OpenSea.svg";
 import Metamask from "../../Images/Metamask.svg";
